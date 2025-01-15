55d951fb8b805476782a05125ce83ee2b924ec7c
Shocker <donaldesset04@outlook.com>
2025-01-15 13:22:03 +0100

    progress bar added

--- app/inference.py
+++ app/inference.py
@@ -2,6 +2,8 @@ import os
 os.environ['SM_FRAMEWORK'] = 'tf.keras'
 
 import numpy as np
+import streamlit as st
+import tqdm
 import keras
 import tensorflow as tf
 import segmentation_models as sm
@@ -15,6 +17,9 @@ image_size = (128, 128)
 preprocess_input = sm.get_preprocessing('resnet18')
 loaded_model = keras.models.load_model('../unet_model_2.keras')
 
+def percentage_func(part, whole):
+    return int(100 * (float(part) / float(whole)))
+
 
 def predictVol(volume):
     dimz, dimx, dimy = volume.shape
@@ -26,12 +31,15 @@ def predictVol(volume):
     volume = volume * 255
 
     count = 0
-
+    progress_counter = 0
+    total_slices = dimx + dimy
+    progress_bar = st.progress(0)
+    status_text = st.empty()
 
     if x_axis:
         print("[+] slicing x axis")
         count += 1
-        for i in range(dimx):
+        for i in tqdm.tqdm(range(dimx), desc="Slicing x axis"):
             slice_2d = volume[:, i, :][..., tf.newaxis]
             slice_2d = tf.image.resize(slice_2d, image_size)
             slice_2d = preprocess_input(slice_2d)
@@ -39,12 +47,16 @@ def predictVol(volume):
             pred = tf.where(pred > 0.5, 1, 0)
             pred = tf.image.resize(pred, (dimz, dimy))
             outimgx[:, i, :] = pred[0, :, :, 0]
+            progress_counter += 1
+            percentage_com = percentage_func(progress_counter, total_slices)
+            progress_bar.progress(percentage_com)
+            status_text.text(f"Progress: {percentage_com}%")
 
 
     if y_axis:
         print("[+] slicing y axis")
         count += 1
-        for i in range(dimy):
+        for i in tqdm.tqdm(range(dimy), desc="Slicing y axis"):
             slice_2d = volume[:, :, i][..., tf.newaxis]
             slice_2d = tf.image.resize(slice_2d, image_size)
             slice_2d = preprocess_input(slice_2d)
@@ -52,13 +64,16 @@ def predictVol(volume):
             pred = tf.where(pred > 0.5, 1, 0)
             pred = tf.image.resize(pred, (dimz, dimx))
             outimgy[:, :, i] = pred[0, :, :, 0]
-
+            progress_counter += 1
+            percentage_com = percentage_func(progress_counter, total_slices)
+            progress_bar.progress(percentage_com)
+            status_text.text(f"Progress: {percentage_com}%")
 
 
     if z_axis:
         print("[+] slicing z axis")
         count += 1
-        for i in range(dimz):
+        for i in tqdm.tqdm(range(dimz), desc="Slicing z axis"):
             volume = normalize_vol(volume)
             slice_2d = volume[i, :, :][..., tf.newaxis]
             slice_2d = tf.image.resize(slice_2d, image_size)
@@ -67,6 +82,10 @@ def predictVol(volume):
             pred = tf.where(pred > 0.5, 1, 0)
             pred = tf.image.resize(pred, (dimx, dimy))
             outimgz[i, :, :] = pred[0, :, :, 0]
+            progress_counter += 1
+            percentage_com = percentage_func(progress_counter, total_slices)
+            progress_bar.progress(percentage_com)
+            status_text.text(f"Progress: {percentage_com}%")
 
     outimg = (outimgx + outimgy + outimgz) / count
 
